e03f0e72e70cc845494cd1731f704ae70efd7b10
Amanda Jayapurna <44786639+amandajayapurna@users.noreply.github.com>
2022-08-11 13:12:50 -0500

    Update init.py
    Initial init.py file setup

--- init.py
+++ init.py
@@ -1,1 +1,14 @@
+#  ----------------------------------------------------------------
+#   init.py
+#   Version: 1.0.0
+#   Last Updated: August 11, 2022
+#   Author: Amanda Jayapurna
+#  ----------------------------------------------------------------
+
+#  ----- DEFINE CUSTOM FOLDER STRUCTURE ---------------------------
+
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./python')
+
+
 
