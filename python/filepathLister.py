ed5d481b4afa62f9a2be4421f28ba2e4ebff7d46
Amanda Jayapurna <44786639+amandajayapurna@users.noreply.github.com>
2022-08-24 23:38:06 -0500

    added week 5 challenge

--- python/filepathLister.py
+++ python/filepathLister.py
@@ -1,9 +1,9 @@
 #  ---------------------------------------------------------------------------
 #   filepathLister.py
-#   Version: 1.0.0
-#   Last Updated: August 16, 2022
+#   Version: 1.0.4
+#   Last Updated: August 22, 2022
 #   Author: Amanda Jayapurna
-#   ! Python for Nuke Class by Ben McEwan | Class 05
+#   ! Python for Nuke Class by Ben McEwan | Class 05 + Homework Challenge
 #  ---------------------------------------------------------------------------
 
 #  ---------------------------------------------------------------------------
@@ -42,5 +42,19 @@ def file_lister():
 
 
 
+# assign file with scriptname to variable then open
+script_name = os.path.basename(nuke.root()['name'].value())
+output_file = open("C:\\Users\\ajayapurna\\Documents\\"+script_name+"_file_lister_output.txt", "w+")
+# w+" argument says: "create the document if it doesn't exist, or overwrite if it does"
+
+ #add to utilities menu
+utilitiesMenu = nuke.menu('Nuke').addMenu('Utilities')
+utilitiesMenu.addCommand('Filepath Lister', "filepathLister.file_lister()", "ctrl+shift+f", shortcutContext=2)
 
 
+# Replace any instance of 'print' with output_file.write("string_to_write_to_file") function to write text to file.
+output_file.write("Nuke Script: "+script_name)
+output_file.write("\n\nFILE & VERSION LIST:\n")
+
+# ...and also at the end of our for loop
+#output_file.write("\nYou are using "+version_number+" of "+filename_no_version)
